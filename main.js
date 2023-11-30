7862594c1c186d94e84e6ec3cf84dcbb2c06f371
luizfrezende <luiz_rezende123@hotmail.com>
2023-11-30 17:05:26 -0300

    mains funcionando

--- main.js
+++ main.js
@@ -1,13 +1,54 @@
-import React, { useState } from 'react';
+/* PÁGINA INICIAL, LOGIN, CADASTRO */
 
-const App = () => {
-    const [mainVisivel, mainVisivelEscolhida] = useState(1);
+document.addEventListener("DOMContentLoaded", function () {
+    // Função para mostrar uma main e ocultar as outras
+    function exibirMain(mainParaExibir) {
+        var mains = document.querySelectorAll("main");
+        mains.forEach(function (main) {
+            main.style.display = (main === mainParaExibir) ? "block" : "none";
+        });
+    }
 
-    const mostrarMain = (numeroMain) => {
-        mainVisivelEscolhida(numeroMain);
-    };
+    // Adicionar ouvintes de eventos aos botões
+    document.getElementById("botaoPaginaInicio").addEventListener("click", function () {
+        exibirMain(document.getElementById("paginaInicial"));
+    });
 
-    return (mostrarMain);
-}
+    document.getElementById("botaoPaginaEntrar").addEventListener("click", function () {
+        exibirMain(document.getElementById("paginaLogin"));
+    });
 
-export default App;
+    document.getElementById("botaoPaginaCadastrar").addEventListener("click", function () {
+        exibirMain(document.getElementById("paginaCadastro"));
+    });
+});
+
+/* PÁGINA INICIAL, LOGIN, CADASTRO */
+
+
+/* PÁGINA MENU, FILMES, AVALIAÇÕES */
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Função para mostrar uma main e ocultar as outras
+    function exibirMain(mainParaExibir) {
+        var mains = document.querySelectorAll("main");
+        mains.forEach(function (main) {
+            main.style.display = (main === mainParaExibir) ? "block" : "none";
+        });
+    }
+
+    // Adicionar ouvintes de eventos aos botões
+    document.getElementById("homeBotao").addEventListener("click", function () {
+        exibirMain(document.getElementById("home-page"));
+    });
+
+    document.getElementById("moviesBotao").addEventListener("click", function () {
+        exibirMain(document.getElementById("allMovies"));
+    });
+
+    document.getElementById("ratingsBotao").addEventListener("click", function () {
+        exibirMain(document.getElementById("allRatings"));
+    });
+});
+
+/* PÁGINA MENU, FILMES, AVALIAÇÕES */
